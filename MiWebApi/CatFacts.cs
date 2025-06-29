cac9109db49a7129ebcf93c4356d0002f3a8f481
Diego Aredes <aredesdiegofabian@gmail.com>
2025-06-29 19:20:06 -0300

    Completado el punto 3

---
+++ MiWebApi/CatFacts.cs
@@ -0,0 +1,8 @@
+ namespace Manejo.CatFacts
+ {
+    public class CatFacts
+    {
+        public string fact{ get; set;}
+        public int length { get; set;}
+    }
+ }
---
+++ MiWebApi/Program.cs
@@ -0,0 +1,61 @@
+﻿using Manejo.CatFacts;
+using System.Text.Json;
+
+var gato = await GetGato();
+
+Console.WriteLine("FACTS: " + gato.fact);
+Console.WriteLine("LENGTH: " + gato.length);
+
+const string NombreArchivo = "CAT-FACT.json";
+var miHelper = new HelperDeJsonGatos();
+
+Console.WriteLine("--Serializando--");
+var options = new JsonSerializerOptions { WriteIndented = true };
+string gatoJson = JsonSerializer.Serialize(gato, options);
+Console.WriteLine("Archivo Serializado:\n" + gatoJson);
+
+Console.WriteLine("--Guardando--");
+miHelper.GuardarArchivoTexto(NombreArchivo, gatoJson);
+
+Console.WriteLine("--Abriendo--");
+string jsonDocument = miHelper.AbrirArchivoTexto(NombreArchivo);
+
+if (string.IsNullOrWhiteSpace(jsonDocument))
+{
+    Console.WriteLine("El archivo está vacío o no se pudo leer.");
+    return;
+}
+
+Console.WriteLine("--Deserializando--");
+try
+{
+    var GatoRecuperado = JsonSerializer.Deserialize<CatFacts>(jsonDocument);
+    Console.WriteLine("--Mostrando datos recuperados--");
+    Console.WriteLine($"{GatoRecuperado.fact} - {GatoRecuperado.length}");
+}
+catch (JsonException ex)
+{
+    Console.WriteLine("Error al deserializar: " + ex.Message);
+}
+
+static async Task<CatFacts> GetGato()
+{
+    var url = "https://catfact.ninja/fact";
+
+    try
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        CatFacts fact = JsonSerializer.Deserialize<CatFacts>(responseBody);
+        return fact;
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine("Problemas de acceso a la API");
+        Console.WriteLine("Mensaje: " + e.Message);
+        return null;
+    }
+}
